b61d5cd78d6e629439de2b5c8513c92824440f24
Martin <bekzodsanakulov89@gmail.com>
2021-06-11 12:21:55 +0900

    feat: collection of coin ticker price data via web sockets on db

--- coins_exec/bitcoin.js
+++ coins_exec/bitcoin.js
@@ -1,19 +1,31 @@
 let SocketConnect = require('../models/socketConnect');
-// mysql db connection
+const moment = require('moment')
 const MySql = require('../models/mysql2');
 const db = new MySql();
 
 // Connection to binance websocket connection
-let bitcoin_ws = new SocketConnect("wss://dex.binance.org/api/ws/BNB_BTCB-1DE@kline_1m");
-bitcoin_ws.collect((err, data) => {
-    if(err) console.log(err);
+// let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/ws/btcbusd@ticker");
+// let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker");
+let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker/ethbusd@ticker/bnbbusd@ticker/adabusd@ticker/dogebusd@ticker");
+
+
+bitcoin_ws.collect(async (err, response) => {
+    if (err) console.log(err);
     else {
-        console.log(`The TYPE: ${data.type}`);
-        console.log(data.data);
+        console.log(`The TYPE: ${response.type}`);
+        const data = response.data['data'];
+        ticksRecord(data);
     }
 });
 
-// db.getCoinData().then(response => {
-//     console.log('** HERE on DB **');
-//     console.log(response);
-// }).catch(err => console.log('ERROR: ', err));
+
+function ticksRecord(data) {
+    const current_time = moment().utc().set('millisecond', 0).format("YYYY-MM-DD hh:mm:ss");
+    const tick_obj = {pairs: data['s'], time: current_time, data: data};
+    console.log('TICKER: ', tick_obj);
+    db.insertCoinData(tick_obj).then((data) => {
+        console.log('result: ', data);
+    }).catch((err) => {
+        console.log('ERROR: ', err);
+    });
+}
--- models/mysql2.js
+++ models/mysql2.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 class MySql {
     constructor() {
-        this.con = null
+        this.con = null;
     }
 
     async connection() {
@@ -16,9 +16,48 @@ class MySql {
     }
 
     async getCoinData() {
-        if (!this.con) await this.connection()
+        if (!this.con) await this.connection();
         const query_result = await this.con.query('select * from bitcoin_price_collection');
         return query_result[0][0];
+    }
+
+    async insertCoinData(ticker) {
+        try {
+            if (!this.con) await this.connection();
+            let table = '';
+            switch(ticker.pairs) {
+                case 'BTCBUSD':
+                    table = 'bitcoin_tick_collection';
+                    break;
+                case 'ETHBUSD':
+                    table = 'ethereum_tick_collection';
+                    break;
+                case 'BNBBUSD':
+                    table = 'binance_tick_collection';
+                    break;
+                case 'ADABUSD':
+                    table = 'cardano_tick_collection';
+                    break;
+                case 'DOGEBUSD':
+                    table = 'doge_tick_collection';
+                    break;
+                default:
+                    throw new Error('unplanned pairs');
+                    break;
+            }
+
+            if(table === 'bitcoin_tick_collection') {
+                let sql = 'INSERT INTO bitcoin_tick_collection SET binstamp = ?, coltime = ?, close = ?, open = ?, high=?, low = ?, volume =?'
+                const query_result = await this.con.query(sql, [ticker.data['E'], ticker.time, ticker.data['c'], ticker.data['o'], ticker.data['h'], ticker.data['l'], ticker.data['v']]);
+                return query_result[0][0];
+            } else {
+                return ticker.pairs;
+            }
+
+        } catch(err) {
+            throw err;
+        }
+
     }
 
 }
