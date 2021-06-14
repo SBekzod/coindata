fa2c7edca862177b280eb804e6db041e61f53e77
Martin <bekzodsanakulov89@gmail.com>
2021-06-14 11:35:34 +0900

    feat: checking the data receive and reconnection to sockets

--- coins_exec/bitcoin.js
+++ coins_exec/bitcoin.js
@@ -3,25 +3,45 @@ const moment = require('moment')
 const MySql = require('../models/mysql2');
 const db = new MySql();
 
-
 // Connection to binance websocket connection to collect BITCOIN data
 let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker");
+// Connection to binance websocket connection to collect other four coins data
+let coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/bnbbusd@ticker/adabusd@ticker/dogebusd@ticker");
+
+
+let period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+let period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+setInterval(function() {
+    if(period_btc < moment.utc().add( -10, 'seconds').format("YYYY-MM-DD hh:mm:ss")) {
+        period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+        bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker");
+    } else if(period_others < moment.utc().add( -10, 'seconds').format("YYYY-MM-DD hh:mm:ss")) {
+        period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+        coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/bnbbusd@ticker/adabusd@ticker/dogebusd@ticker");
+    } else {
+        console.log(period_btc);
+        console.log('---------------------');
+    }
+}, 5000);
+
+
 bitcoin_ws.collect(async (err, response) => {
     if (err) console.log(err);
     else {
+        period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
         console.log(`The MAIN TYPE: ${response.type} and Pairs: ${response.data['data']['s']}`);
+        console.log('---------------------');
         const data = response.data['data'];
         ticksRecord(data);
     }
 });
 
-
-// Connection to binance websocket connection to collect other four coins data
-let coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/bnbbusd@ticker/adabusd@ticker/dogebusd@ticker");
 coin_others.collect(async (err, response) => {
     if (err) console.log(err);
     else {
+        period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
         console.log(`The OTHER TYPE: ${response.type} and Pairs: ${response.data['data']['s']}`);
+        console.log('---------------------');
         const data = response.data['data'];
         ticksRecord(data);
     }
