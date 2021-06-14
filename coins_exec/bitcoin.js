fd272d9a627cbd820eca52ecc378199315734a7b
Martin <bekzodsanakulov89@gmail.com>
2021-06-14 10:24:00 +0900

    fix: deviding the coin web sockets connections

--- coins_exec/bitcoin.js
+++ coins_exec/bitcoin.js
@@ -3,16 +3,25 @@ const moment = require('moment')
 const MySql = require('../models/mysql2');
 const db = new MySql();
 
-// Connection to binance websocket connection
-// let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/ws/btcbusd@ticker");
-// let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker");
-let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker/ethbusd@ticker/bnbbusd@ticker/adabusd@ticker/dogebusd@ticker");
 
-
+// Connection to binance websocket connection to collect BITCOIN data
+let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker");
 bitcoin_ws.collect(async (err, response) => {
     if (err) console.log(err);
     else {
-        console.log(`The TYPE: ${response.type}`);
+        console.log(`The MAIN TYPE: ${response.type} and Pairs: ${response.data['data']['s']}`);
+        const data = response.data['data'];
+        ticksRecord(data);
+    }
+});
+
+
+// Connection to binance websocket connection to collect other four coins data
+let coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/bnbbusd@ticker/adabusd@ticker/dogebusd@ticker");
+coin_others.collect(async (err, response) => {
+    if (err) console.log(err);
+    else {
+        console.log(`The OTHER TYPE: ${response.type} and Pairs: ${response.data['data']['s']}`);
         const data = response.data['data'];
         ticksRecord(data);
     }
@@ -22,9 +31,10 @@ bitcoin_ws.collect(async (err, response) => {
 function ticksRecord(data) {
     const current_time = moment(data['E']).utc().format("YYYY-MM-DD hh:mm:ss");
     const tick_obj = {pairs: data['s'], col_time: current_time, data: data};
-    console.log('TICKER: ', tick_obj);
-    db.insertCoinData(tick_obj).then((data) => {
-        console.log('result: ', data);
+    // console.log('TICKER: ', tick_obj);
+    db.insertCoinData(tick_obj).then((resp_data) => {
+        console.log(`** result of pairs ${data['s']} => , ${resp_data} **`);
+        console.log('---------------------');
     }).catch((err) => {
         console.log('ERROR: ', err);
     });
--- server.js
+++ server.js
@@ -6,14 +6,11 @@ const shell = require('shelljs');
 const dotenv = require('dotenv');
 dotenv.config({path: process.env.NODE_ENV === 'PRODUCTION' ? './.env.prod' : './.env.dev'});
 
-// bitcoin
+// BITCOIN and OTHERS
 setTimeout(() => shell.exec('node coins_exec/bitcoin.js', {async: true}), 5000);
 
-// etherium
-// setTimeout(() => shell.exec('node coins_exec/etherium.js'), 10000);
-
-// doge
-// setTimeout(() => shell.exec('node coins_exec/doge.js'), 15000);
+// OTHER PAIRS: ethereum, binance, cardano and doge
+// setTimeout(() => shell.exec('node coins_exec/others_coins.js', {async: true}), 10000);
 
 
 // EXPRESS SERVER CONNECTION
