513b1f9cadf30949af2b8eb8fe69d2b5b6a2a8c1
Martin <bekzodsanakulov89@gmail.com>
2021-06-17 11:37:56 +0900

    fix: refactoring

--- coins_exec/bitcoin.js
+++ coins_exec/bitcoin.js
@@ -3,41 +3,10 @@ const moment = require('moment')
 const MySql = require('../models/mysql2');
 let db = new MySql();
 
-let bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker"),
-    coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/dogebusd@ticker"),
-    period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss"),
-    period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+let bitcoin_ws, coin_others, period_btc, period_others;
+creatingPairsSocket('main');
+creatingPairsSocket('others');
 
-bitcoin_ws.collect(async (err, response) => {
-    if (err) {
-        console.log(`${err} on main`);
-    } else {
-        period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
-        const data = response.data['data'];
-        if (await ticksRecord(data)) {
-            console.log(`main ${response.type}, pairs: ${response.data['data']['s']} at ${period_btc}`);
-            console.log('---------------------');
-        } else {
-            console.log(`NOT UPDATED MAIN ${moment.utc().format("YYYY-MM-DD hh:mm:ss")}`);
-            console.log('---------------------');
-        }
-    }
-});
-coin_others.collect(async (err, response) => {
-    if (err) {
-        console.log(`${err} on others`);
-    } else {
-        period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
-        const data = response.data['data'];
-        if (await ticksRecord(data)) {
-            console.log(`other ${response.type}, pairs: ${response.data['data']['s']} at ${period_others}`);
-            console.log('---------------------');
-        } else {
-            console.log(`NOT UPDATED OTHERS ${moment.utc().format("YYYY-MM-DD hh:mm:ss")}`);
-            console.log('---------------------');
-        }
-    }
-});
 async function ticksRecord(data) {
     try {
         const current_time = moment(data['E']).utc().format("YYYY-MM-DD hh:mm:ss");
@@ -47,69 +16,85 @@ async function ticksRecord(data) {
     } catch (err) {
         console.log('ERROR: ', err);
         console.log('---------------------');
-        // recall reconnection on db
+        // recall reconnection on db from next insertion
         db.con = null;
         return false;
     }
 }
 
+function creatingPairsSocket(pairs_type, isPrimary = true) {
+    if(pairs_type === 'main') {
+        bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker"),
+            period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
 
-//TODO: NOTICE!!! BINANCE FREE WEBSOCKET did not respond with standard echo-protocol codes
-// and neither with close or error messages after primary connection
-// customized solution: handling with socket muted state
+        bitcoin_ws.collect(async (err, response) => {
+            let order = (isPrimary === false) ? 'REC:' : '';
+            if (err) {
+                console.log(`${order} ${err} on ${pairs_type}`);
+                console.log('===========');
+            } else {
+                period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+                const data = response.data['data'];
+                if (await ticksRecord(data)) {
+                    console.log(`${order} main ${response.type}, pairs: ${response.data['data']['s']} at ${period_btc}`);
+                    console.log('---------------------');
+                } else {
+                    console.log(`${order} NOT UPDATED MAIN ${moment.utc().format("YYYY-MM-DD hh:mm:ss")}`);
+                    console.log('---------------------');
+                }
+            }
+        });
+
+    } else {
+        coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/dogebusd@ticker"),
+            period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+
+        coin_others.collect(async (err, response) => {
+            let order = (isPrimary === false) ? 'REC:' : '';
+            if (err) {
+                console.log(`${order} ${err} on ${pairs_type}`);
+                console.log('===========');
+            } else {
+                period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
+                const data = response.data['data'];
+                if (await ticksRecord(data)) {
+                    console.log(`${order} others ${response.type}, pairs: ${response.data['data']['s']} at ${period_others}`);
+                    console.log('---------------------');
+                } else {
+                    console.log(`${order} NOT UPDATED OTHERS ${moment.utc().format("YYYY-MM-DD hh:mm:ss")}`);
+                    console.log('---------------------');
+                }
+            }
+        });
+    }
+}
+
+
+//TODO: JUST NOTICE!!!
+    // BINANCE FREE WEBSOCKET did not respond with standard echo-protocol codes
+    // and neither with close or error messages after primary connection
+    // customized solution: handling with socket muted state
+
 setInterval(function () {
     try {
         console.log(`☄ checker ${moment.utc().format("YYYY-MM-DD hh:mm:ss")} main:${period_btc} others:${period_others} ☄`);
 
         if (period_btc < moment.utc().add(-20, 'seconds').format("YYYY-MM-DD hh:mm:ss")) {
-            console.log(`******* RECONNECT MAIN: ${period_btc} *******`);
             period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
-
             bitcoin_ws.closeSocketConnection();
-            bitcoin_ws = new SocketConnect("wss://stream.binance.com:9443/stream?streams=btcbusd@ticker");
-            bitcoin_ws.collect(async (err, response) => {
-                if (err) {
-                    console.log(`REC: ${err} on main`);
-                } else {
-                    period_btc = moment.utc().format("YYYY-MM-DD hh:mm:ss");
-                    const data = response.data['data'];
-                    if (await ticksRecord(data)) {
-                        console.log(`REC: main ${response.type}, pairs: ${response.data['data']['s']} at ${period_btc}`);
-                        console.log('---------------------');
-                    } else {
-                        console.log(`REC: NOT UPDATED MAIN ${moment.utc().format("YYYY-MM-DD hh:mm:ss")}`);
-                        console.log('---------------------');
-                    }
-                }
-            });
+            setTimeout(() => creatingPairsSocket('main', false), 1000);
         } else {
             console.log(`******* NORMAL MAIN CONNECTION: ${period_btc} *******`);
         }
 
         if (period_others < moment.utc().add(-20, 'seconds').format("YYYY-MM-DD hh:mm:ss")) {
-            console.log(`******* RECONNECT OTHERS: ${period_others} *******`);
-
             period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
             coin_others.closeSocketConnection();
-            coin_others = new SocketConnect("wss://stream.binance.com:9443/stream?streams=ethbusd@ticker/dogebusd@ticker");
-            coin_others.collect(async (err, response) => {
-                if (err) {
-                    console.log(`REC: ${err} on others`);
-                } else {
-                    period_others = moment.utc().format("YYYY-MM-DD hh:mm:ss");
-                    const data = response.data['data'];
-                    if (await ticksRecord(data)) {
-                        console.log(`REC: other ${response.type}, pairs: ${response.data['data']['s']} at ${period_others}`);
-                        console.log('---------------------');
-                    } else {
-                        console.log(`REC: NOT UPDATED OTHERS ${moment.utc().format("YYYY-MM-DD hh:mm:ss")}`);
-                        console.log('---------------------');
-                    }
-                }
-            });
+            setTimeout(() => creatingPairsSocket('others', false), 1000);
         } else {
             console.log(`******* NORMAL OTHERS CONNECTION: ${period_others} *******`);
         }
+        console.log('=====================');
 
     } catch (err) {
         console.log(`@@ UNDEFINED ERROR: ${err}`)
--- models/socketConnect.js
+++ models/socketConnect.js
@@ -7,14 +7,14 @@ class SocketConnect {
 
     constructor(url = 'wss://echo.websocket.org') {
         this.url = url;
+        this.clientConnection = null;
         console.log('URL: ', this.url);
-        this.client = null;
     }
 
     collect(callback) {
 
-        this.client = new WebSocketClient();
-        this.client.connect(this.url);
+        let client = new WebSocketClient();
+        client.connect(this.url);
 
         // customized: checking data receipt
         let timer = 0;
@@ -22,30 +22,30 @@ class SocketConnect {
             timer++;
         }, 1000);
 
-        this.client.on('connectFailed', (error) => {
+        client.on('connectFailed', (error) => {
             console.log('Connect Error: ' + error);
             callback(error, null);
         })
 
-        this.client.on('connect', (connection) => {
+        client.on('connect', (connection) => {
             console.log('WebSocket Client Connected');
+            this.clientConnection = connection;
 
-            connection.on('error', (error) => {
+            this.clientConnection.on('error', (error) => {
                 console.log("Connection Error: " + error.toString());
-                callback('error in connection', null);
+                callback('LOGGER: error in connection', null);
             })
 
-            connection.on('close', (data) => {
-                console.log('echo-protocol Connection Closed');
-                callback('close in connection', null);
+            this.clientConnection.on('close', (data) => {
+                callback('LOGGER: echo-protocol Connection Closed', null);
             })
 
-            connection.on('message', (event) => {
+            this.clientConnection.on('message', (event) => {
                 timer = 0;
                 let message = JSON.parse(event.utf8Data);
                 callback(null, {type: "msg", data: message});
 
-                // customized: checking whether msg is received in the next five seconds duration
+                // Checking whether msg is received in the next five seconds duration
                 setTimeout(() => {
                     if (timer >= 5) {
                         let coin_type = (message['data']['s'] === "BTCBUSD") ? 'BTC' : 'ETH/DOGE';
@@ -58,11 +58,14 @@ class SocketConnect {
                         }).then(() => console.log('recorded'))
                             .catch(err => console.log(`Record error: ${err}`));
                         timer = 0;
+
+                        // Closing Client Connection
                         this.closeSocketConnection();
                         clearInterval(computing);
-                        callback('comets forced to close', null);
+                        callback('LOGGER: comets forced launched close order', null);
                     }
                 }, 5000);
+
             })
 
         })
@@ -70,8 +73,7 @@ class SocketConnect {
     }
 
     closeSocketConnection() {
-        this.client.close();
-        this.client = null;
+        this.clientConnection.close();
     }
 
 }
